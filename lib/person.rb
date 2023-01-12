ce841402539e4094bb411006201c326d19964b7e
Alejandro <alejelias@gmail.com>
2023-01-12 17:19:27 -0500

    commit tree 2bdd158e43d1aa713264a3532d4108825891d891

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,35 @@
 # your code goes here
+#learn test
+require 'pry'
+class Person
+    puts "I am alive"
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25, hygiene = 8, happiness = 10)
+        @name = name
+        @hygiene = hygiene
+        @bank_account= bank_account
+        @happiness= happiness
+
+    end
+
+    def get_paid(amount)
+        bank_account + amount
+        return "all about the benjamins"
+    end
+    def take_bath()
+        @hygiene + 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    def work_out()
+        happiness + 2
+       return  "♪ another one bites the dust ♫"
+    end
+
+end
+
+
+new_person = Person.new("alejandro")
+
+#binding.pry
